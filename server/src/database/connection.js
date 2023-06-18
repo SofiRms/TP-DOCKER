56c6c28bf593e4cc6066b156354ebca08cecdd8f
sofi <sofiamagaliramos@gmail.com>
2023-06-18 17:50:34 -0300

    agregué comentarios

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql')
 
+//las variables de entorno están definidas en el archivo compose
 const pool = mysql.createConnection({
+
     host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'user', 
+    user: process.env.DB_USER || 'root', 
     password: process.env.DB_PASSWORD || 'mypassword', 
     database: process.env.DB_NAME || 'libros',
     connectionLimit: 5, 
 });
 
 
-pool.connect(err => {    user: 'root', 
-
+pool.connect(err => { user: 'root', 
     err
         ? console.error(`algo salió mal: ${err}`)	
         : console.log(`connection ready`)
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -5,6 +5,6 @@ const router = require('express').Router()
 router.get('/libros', getAllBooks)
 router.post('/libros',insertarLibro)
 
-// router.post('/libros', post)
+
 
 module.exports = router

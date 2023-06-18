d1543c6a363121ee1b1d6b47b589b3f5563a9789
sofi <sofiamagaliramos@gmail.com>
2023-06-18 20:42:35 -0300

    ready

--- server/app.js
+++ server/app.js
@@ -7,16 +7,9 @@ require('dotenv').config();
 const PORT = process.env.PORT 
 const app = express()
 
-
-//para comprobar nms
-console.log(process.env.DB_HOST)
-
-
 app.use(cors())
 app.use(morgan('combined'))
-
 app.use(express.json())
-
 app.use(require('./src/routes/routes.js'))
 
 app.listen(PORT, () => 
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,10 +1,11 @@
-const {getAllBooks,insertarLibro} = require('../controllers/libros')
+const {getAllBooks,insertBook} = require('../controllers/libros')
 
 const router = require('express').Router()
 
 router.get('/libros', getAllBooks)
-router.post('/libros',insertarLibro)
-
-
+router.post('/libros',insertBook)
 
 module.exports = router
+
+
+
